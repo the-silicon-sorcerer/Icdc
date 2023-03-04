ec09c645f42572ff61aeb5a715677b9fd77daef8
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-03-03 22:57:28 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@
 
 import moment from "moment";
 import style from "./page.module.css";
+import { useState } from "react";
 
 const MainPage = () => {
+  const html = document.querySelector("html");
+  const onClick = () => {
+    html?.style.backgroundColor === "rgb(99, 143, 255)"
+      ? "red"
+      : "rgb(99, 143, 255)";
+  };
+
   const daysLeft = () => {
     const eventDay = moment("2023-04-20");
     return eventDay.diff(moment(), "days");
@@ -21,6 +29,7 @@ const MainPage = () => {
           <div
             className={style.progress}
             style={{ width: `${calcPrecentage(daysLeft(), 50)}%` }}
+            onClick={() => onClick()}
           ></div>
         </div>
       </div>
